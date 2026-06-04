786b86cb9a00ea7380d03bb5d148b908de431ef4
chrischall <chris.c.hall@gmail.com>
2026-06-04 00:28:40 -0400

    chore(main): release 1.1.5 (#56)
    :robot: I have created a release *beep* *boop*
    ---
    
    
    ##
    [1.1.5](https://github.com/chrischall/canvas-parent-mcp/compare/v1.1.4...v1.1.5)
    (2026-06-04)
    
    
    ### Bug Fixes
    
    * adopt @fetchproxy/server 0.13.0 (bridge host failover + re-pairing)
    ([#55](https://github.com/chrischall/canvas-parent-mcp/issues/55))
    ([f3f1dc0](https://github.com/chrischall/canvas-parent-mcp/commit/f3f1dc0d6029db4693f1a6c3142e0e26b1c1b2b9))
    * adopt @fetchproxy/server 1.0.0 + @chrischall/mcp-utils 0.5.0
    ([#57](https://github.com/chrischall/canvas-parent-mcp/issues/57))
    ([b34c774](https://github.com/chrischall/canvas-parent-mcp/commit/b34c7748687f89e4e9af8c550fc4792bf86b0e4a))
    
    ---
    This PR was generated with [Release
    Please](https://github.com/googleapis/release-please). See
    [documentation](https://github.com/googleapis/release-please#release-please).

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ try {
 // host's install-time `tools/list` succeeds.
 await runMcp({
   name: 'canvas',
-  version: '1.1.4', // x-release-please-version
+  version: '1.1.5', // x-release-please-version
   tools: [
     (server) => {
       if (resolved) {
